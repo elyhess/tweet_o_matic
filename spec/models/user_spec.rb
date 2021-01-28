8878eafd305bda115efbc23f42df91456a2eeb17
Ely Hess <Ely.hess@me.com>
2021-01-28 10:22:58 -0700

    Add bcrypt

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
-require 'rails_helper'
+RSpec.describe , type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :}
+  end
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it {should belong_to :}
+    it {should have_many :}
+  end
+
+  describe 'instance methods' do
+  end
+
+  describe 'class methods' do 
+  end
+  
 end
+
