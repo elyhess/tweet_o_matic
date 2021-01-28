213441db45593cfee2c536510bfc1563325c4aab
Ely Hess <Ely.hess@me.com>
2021-01-28 15:20:12 -0700

    Add password resets controller update & edit w/ view

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -6,13 +6,30 @@ class PasswordResetsController < ApplicationController
 	def create
 		@user = User.find_by(email: params[:email])
 		if @user.present?
-			require "pry"; binding.pry
 			PasswordMailer.with(user: @user).reset.deliver_now
 		end
 		redirect_to root_path, notice: "If an account with that email was found, we have sent a link to reset your password."
 	end
 
 	def edit
-		@user = User.find_signed(params[:token], purpose: "password_reset")
+		@user = User.find_signed!(params[:token], purpose: "password_reset")
+	rescue ActiveSupport::MessageVerifier::InvalidSignature
+		redirect_to sign_in_path, alert: "Your token has expired please try again."
 	end
+
+	def update
+		@user = User.find_signed!(params[:token], purpose: "password_reset")
+		if @user.update(password_params)
+			redirect_to sign_in_path, notice: "Your password was reset successfully. Please sign in."
+		else
+			render :edit
+		end
+	end
+
+	private
+
+	def password_params
+		params.require(:user).permit(:password, :password_confirmation)
+	end
+
 end
