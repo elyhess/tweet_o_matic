e69ca62fdf7bf2be4c349c294ffb44d617c6a5c5
Ely Hess <Ely.hess@me.com>
2021-01-28 11:08:34 -0700

    Add user new & create w/ views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 	root 'welcome#index'
 	resources :about, only: [:index]
+
+	get 'sign_up', to: 'registrations#new'
+	post 'sign_up', to: 'registrations#create'
 end
