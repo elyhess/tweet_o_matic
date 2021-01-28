22c56314f345e15e65ec31285f14553994c83255
Ely Hess <Ely.hess@me.com>
2021-01-28 10:35:45 -0700

    Change SQLite => postgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_01_28_171926) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
