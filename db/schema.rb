90a7341322eafeb51c46fdba460cf12496067f3a
Ely Hess <Ely.hess@me.com>
2021-01-28 10:37:05 -0700

    Modify migration null: true

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2021_01_28_171926) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
+    t.string "email", null: false
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
