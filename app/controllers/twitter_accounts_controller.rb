4cf600536df6fcefa61518dabf66eb66a78c9f7f
Ely Hess <Ely.hess@me.com>
2021-01-29 10:41:38 -0700

    Add account destroy link & action

--- app/controllers/twitter_accounts_controller.rb
+++ app/controllers/twitter_accounts_controller.rb
@@ -5,4 +5,10 @@ class TwitterAccountsController < ApplicationController
 		@twitter_accounts = Current.user.twitter_accounts
 	end
 
+	def destroy
+		twitter_account = Current.user.twitter_accounts.find(params[:id])
+		twitter_account.destroy
+		redirect_to twitter_accounts_path
+	end
+
 end
