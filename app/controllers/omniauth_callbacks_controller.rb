befd4d03c1a638672118d2c9b789776c3944503a
Ely Hess <Ely.hess@me.com>
2021-01-29 09:32:18 -0700

    Add omniauth callback controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,4 +1,17 @@
 class OmniauthCallbacksController < ApplicationController
 	def twitter
+		account = Current.user.twitter_accounts.where(username: auth.info.nickname).first_or_initialize
+		account.update(
+			name:     auth.info.name,
+			username: auth.info.nickname,
+			token:    auth.info.image,
+			secret:   auth.credentials.secret
+		)
+		redirect_to root_path, notice: "Successfully connected your account."
 	end
+
+	def auth
+		request.env['omniauth.auth']
+	end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 	include GlobalID::Identification
-
+	has_many :twitter_accounts, dependent: :destroy
 	has_secure_password
 	validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "you must use a valid email address" }
 	validates_presence_of :password
