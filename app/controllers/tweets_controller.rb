3618fab7ea02ac623f09f23389b2b7bc654bf460
Ely Hess <Ely.hess@me.com>
2021-01-29 12:06:57 -0700

    Add create action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,4 +10,18 @@ class TweetsController < ApplicationController
 	end
 
 
+	def create
+		@tweet = Current.user.tweets.create(tweet_params)
+		if @tweet.save
+			redirect_to tweets_path, notice: "Tweet was scheduled successfully"
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def tweet_params
+		params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
+	end
 end
