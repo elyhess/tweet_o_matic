ec605754f35398f0bed124dcaac59f653171f13a
Ely Hess <Ely.hess@me.com>
2021-01-30 17:39:13 -0700

    Add status to AutomaticTweet model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_30_174507) do
+ActiveRecord::Schema.define(version: 2021_01_31_000701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_01_30_174507) do
     t.integer "post_interval"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["twitter_account_id"], name: "index_automated_tweets_on_twitter_account_id"
     t.index ["user_id"], name: "index_automated_tweets_on_user_id"
   end
