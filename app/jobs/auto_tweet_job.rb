da7f54d57f5df7af20d52ff04495bafe249acb31
Ely Hess <ely.hess@me.com>
2021-04-21 11:11:38 -0600

    Remove actioncable broadcast

--- app/jobs/auto_tweet_job.rb
+++ app/jobs/auto_tweet_job.rb
@@ -3,6 +3,8 @@ class AutoTweetJob < ApplicationJob
 
 	def perform(automated_tweet)
 		automated_tweet.publish_to_twitter!
+		ActionCable.server.broadcast "automated_tweets", { html: "<div>This should be appearing somewhere???</div>" }
+		# ActionCable.server.broadcast "automated_tweets"
 	end
 
 end
