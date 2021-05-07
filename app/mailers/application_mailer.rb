4d3d10abf8e8e0eae2052ba170afb700fa7b9b35
Ely Hess <ely.hess@me.com>
2021-05-07 12:38:29 -0600

    Configure actionmailer for production

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: ENV["admin_email"]
   layout 'mailer'
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,6 +74,16 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV["admin_email"],
+    :password             => ENV["admin_password"],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+
   config.active_job.queue_adapter = :sidekiq
 
   config.action_cable.url = "ws://localhost:3000/cable"
